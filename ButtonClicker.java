74c878622d7a350c2bd04a3323242cc17106a066
Blaze0ne <daniel.babot.db@gmail.com>
2025-11-17 04:17:41 +0100

    Add files via upload

---
+++ ButtonClicker.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+
+public class ButtonClicker{
+
+	public static void main(String[] args) {
+		
+
+	}
+//Etape 1: Faire une frame.
+	public class window {
+	
+	//Etape 2: Faire un bouton "Click Me".
+	//Etape 3: mettre un chiffre au-dessus du boutton, qui augmente à 
+	//chaque clique.	
+	}
+	
+}
