2f55f65f8d1e7fac405204855007d40fff46e8f2
Blaze0ne <daniel.babot.db@gmail.com>
2025-11-22 15:33:58 +0100

    Add JLabel to ButtonClicker and adjust frame size

--- ButtonClicker.java
+++ ButtonClicker.java
@@ -7,11 +7,16 @@ public class ButtonClicker{
 		JFrame frame = new JFrame();
 		JButton button = new JButton (" Click Me");
 		button.setBounds(150, 200, 220, 50);
-
+		
+		JLabel number = new JLabel("00");
+		number.setBounds(200, 50, 50, 50);
+		
 		frame.add(button);
-		frame.setSize(500, 600);
+		frame.add(number);
+		frame.setSize(400, 200);
 		frame.setLayout(null);
 		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 	}	
 }
